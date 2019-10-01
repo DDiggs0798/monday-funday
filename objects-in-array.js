9c6a2216bf7abda989932a2ff4d1e55d7ea25833
Dominique Diggs <dominiquediggs@dominiques-air.worcester.local>
2019-09-30 21:43:24 -0400

    Car listings

---
+++ objects-in-array.js
@@ -0,0 +1,18 @@
+var cars = [{
+        make: 'nissan',
+        model: 'rouge',
+        year: 2011
+},     {
+        make:'ford',
+        model: 'explorer',
+        year: 2017
+},      {
+        make: 'honda',
+        model: 'Element',
+        year: 2015
+},      {
+        make: 'Dodge',
+        model: 'ram',
+        year: 2017
+}]       
+console.log(cars)
