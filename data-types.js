32eeca6e899c79e6a98b271e847e5b6f9c04285a
Dominique Diggs <dominiquediggs@dominiques-air.worcester.local>
2019-09-30 21:11:48 -0400

    i tried

---
+++ data-types.js
@@ -0,0 +1,18 @@
+var b = {ball}
+var Pets = [Dog, Cat, Bird]
+var Place = 'Park'
+var Toys = 2;
+var owner ={
+    name = Jeff,
+    age = 21
+}
+var today = new Date('9/30')
+var tomorrow = new Date ('10/1')
+
+function sum(Toys) {
+    var Toys = 2
+    for(var i = 3; i < Toys.length;) {
+        total = total + Toys[i]
+    }
+    return total
+}
